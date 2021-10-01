478849a81e1760387efd57e71343379fb3378187
Aditya Mahes <91634844+adityamahes@users.noreply.github.com>
2021-10-01 18:50:58 -0400

    Update and rename BandNameGenerator.py to Simple.../BandNameGenerator.py

--- BandNameGenerator.py
+++
@@ -1,5 +0,0 @@
-def game():
-    from art import BandNameGeneratorLogo
-    print(BandNameGeneratorLogo)
-    print("Welcome to the Band Name Generator.")
-    print("Your band name could be " + input("What's name of the city you grew up in?\n") + " " + input("What's your pet's name?\n"))
---
+++ Simple.../BandNameGenerator.py
@@ -0,0 +1,4 @@
+from art import BandNameGeneratorLogo
+print(BandNameGeneratorLogo)
+print("Welcome to the Band Name Generator.")
+print("Your band name could be " + input("What's name of the city you grew up in?\n") + " " + input("What's your pet's name?\n")
